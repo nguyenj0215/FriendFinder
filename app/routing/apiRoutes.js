e2e709cfaba3e78dd49590e128dc2b69d68a18b9
Nguyenj0215 <Nguyenj0215@gmail.com>
2019-10-03 20:59:15 -0700

    post method working when new friend is added

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,13 +1,28 @@
-var express = require("express");
-var path = require("path");
-
 var friends = require("./../data/friends.js")
 
-module.exports = function(app){
+module.exports = function(app) {
 
     //Display json data for all friend objects
-    app.get("/api/friends", function(req, res) {
+    app.get("/api/friends", function (req, res) {
         return res.json(friends);
-      });
+    });
 
+    //This should get the new friend information from survey
+    app.post("/api/friends", function (req) {
+        var newFriend = req.body;
+        console.log(newFriend)
+
+        //Loop through the scores array to make strings ints
+        //and reassign values in array
+        for (var i =0;i>newFriend.scores.length; i++) {
+            newFriend.scores[i] = parseInt(newFriend.scores[i])
+        }
+    })
+
+    //Loop through the friends array to calculate compatibility
+    var absDifference = []
+
+    for (var j=0; j<friends.length; j++) {
+        
+    }
 }
--- server.js
+++ server.js
@@ -4,12 +4,12 @@ var path = require("path");
 var app = express();
 var PORT = process.argv.PORT || 8080;
 
-require("./app/routing/apiRoutes.js")(app)
-require("./app/routing/htmlRoutes.js")(app)
-
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+require("./app/routing/apiRoutes.js")(app)
+require("./app/routing/htmlRoutes.js")(app)
 
 //Server listener
 app.listen(PORT, function() {
