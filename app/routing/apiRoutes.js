5a1338c62a92c86839bb2e7a41dd15bd66c28716
Nguyenj0215 <Nguyenj0215@gmail.com>
2019-10-03 22:08:40 -0700

    fixed difference array calculations

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -8,56 +8,57 @@ module.exports = function (app) {
     });
 
     //This should get the new friend information from survey
-    app.post("/api/friends", function (req) {
+    app.post("/api/friends", function (req, res) {
         var newFriend = req.body;
-        console.log(newFriend)
 
         //Loop through the scores array to make strings ints
         //and reassign values in array 
-        for (var i = 0; i > newFriend.scores.length; i++) {
+        for (var i = 0; i < newFriend.scores.length; i++) {
             newFriend.scores[i] = parseInt(newFriend.scores[i])
         }
+
+        //Loop through the friends array to calculate compatibility
+
+        //Array that will contain difference total values 
+        var differenceArray = []
+        //Total difference summed at each index
+        var differenceTotal = 0;
+        //To be used at each index to add to differenceTotal
+        var differenceCurrent;
+
+        //For loop through friends array
+        for (var j = 0; j < friends.length; j++) {
+
+            //For loop for each friends scores (no need to parseint already int in friends.js)
+            for (var k = 0; k < friends[j].scores.length; k++) {
+
+                //Absolute value of current friends score at index - new friend score at same index
+                differenceCurrent = Math.abs(friends[j].scores[k] - newFriend.scores[k]);
+                differenceTotal += differenceCurrent;
+            }
+            //Once looped through all the scores for one friend, push into array before moving to next friend
+            differenceArray.push(differenceTotal)
+        }
+        console.log(differenceArray)
+
+        var smallestIndex = 0;
+        //Loop through differece Array to see which is smallest
+        for (var h = 0; h < differenceArray.length; h++) {
+            //Save first number in array as smallest index
+            if (h === 0) {
+                smallestIndex = differenceArray[h]
+            }
+            //Continue through difference array to find any smaller numbers
+            else if (differenceArray[h] < smallestIndex) {
+                smallestIndex = differenceArray[h]
+            }
+        }
+
+        //push new friend object into friends array
+        friends.push(newFriend)
+
+        //Json response for friend in array that is most compatible
+        res.json(friends[smallestIndex])
+
     })
-
-    //Loop through the friends array to calculate compatibility
-
-    //Array that will contain difference total values 
-    var differenceArray = []
-    //Total difference summed at each index
-    var differenceTotal;
-    //To be used at each index to add to differenceTotal
-    var differenceCurrent;
-
-    //For loop through friends array
-    for (var j = 0; j < friends.length; j++) {
-
-        //For loop for each friends scores (no need to parseint already int in friends.js)
-        for (var k = 0; k < friends[j].scores.length; k++) {
-
-            //Absolute value of current friends score at index - new friend score at same index
-            differenceCurrent = Math.abs(friends.scores[k] - newFriend.scores[j]);
-            differenceTotal += differenceCurrent;
-        }
-        //Once looped through all the scores for one friend, push into array before moving to next friend
-        differenceArray.push(differenceTotal)
-    }
-
-    var smallestIndex = 0;
-    //Loop through differece Array to see which is smallest
-    for (var h=0;h<differenceArray.length;h++) {
-        //Save first number in array as smallest index
-        if (h===0) {
-            smallestIndex = differenceArray[h]
-        }
-        //Continue through difference array to find any smaller numbers
-        else if (differenceArray[h] < smallestIndex) {
-            smallestIndex = differenceArray[h]
-        }
-    }
-
-    //push new friend object into friends array
-    friends.push(newFriend)
-
-    //Json response for friend in array that is most compatible
-    response.json(friends[smallestIndex])
 }
