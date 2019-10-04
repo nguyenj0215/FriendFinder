aed3c52be0afee438e736c35cd97adc35dcb5f32
Nguyenj0215 <Nguyenj0215@gmail.com>
2019-10-03 21:31:06 -0700

    able to find compatible friend

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,6 +1,6 @@
 var friends = require("./../data/friends.js")
 
-module.exports = function(app) {
+module.exports = function (app) {
 
     //Display json data for all friend objects
     app.get("/api/friends", function (req, res) {
@@ -13,16 +13,51 @@ module.exports = function(app) {
         console.log(newFriend)
 
         //Loop through the scores array to make strings ints
-        //and reassign values in array
-        for (var i =0;i>newFriend.scores.length; i++) {
+        //and reassign values in array 
+        for (var i = 0; i > newFriend.scores.length; i++) {
             newFriend.scores[i] = parseInt(newFriend.scores[i])
         }
     })
 
     //Loop through the friends array to calculate compatibility
-    var absDifference = []
 
-    for (var j=0; j<friends.length; j++) {
-        
+    //Array that will contain difference total values 
+    var differenceArray = []
+    //Total difference summed at each index
+    var differenceTotal;
+    //To be used at each index to add to differenceTotal
+    var differenceCurrent;
+
+    //For loop through friends array
+    for (var j = 0; j < friends.length; j++) {
+
+        //For loop for each friends scores (no need to parseint already int in friends.js)
+        for (var k = 0; k < friends[j].scores.length; k++) {
+
+            //Absolute value of current friends score at index - new friend score at same index
+            differenceCurrent = Math.abs(friends.scores[k] - newFriend.scores[j]);
+            differenceTotal += differenceCurrent;
+        }
+        //Once looped through all the scores for one friend, push into array before moving to next friend
+        differenceArray.push(differenceTotal)
     }
+
+    var smallestIndex = 0;
+    //Loop through differece Array to see which is smallest
+    for (var h=0;h<differenceArray.length;h++) {
+        //Save first number in array as smallest index
+        if (h===0) {
+            smallestIndex = differenceArray[h]
+        }
+        //Continue through difference array to find any smaller numbers
+        else if (differenceArray[h] < smallestIndex) {
+            smallestIndex = differenceArray[h]
+        }
+    }
+
+    //push new friend object into friends array
+    friends.push(newFriend)
+
+    //Json response for friend in array that is most compatible
+    response.json(friends[smallestIndex])
 }
