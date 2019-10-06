c7df56b6abd1294102d86610cfdfec91bc7b4025
Nguyenj0215 <Nguyenj0215@gmail.com>
2019-10-05 18:50:05 -0700

    fixed typo in port variable

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var path = require("path");
 
 var app = express();
-var PORT = process.argv.PORT || 8080;
+var PORT = process.env.PORT || 8080;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
